87a63d09320ff506446372315a39cb9082cf806d
Potsawee <potsaweee.p@gmail.com>
2025-03-05 14:41:29 +0700

    create mock data(user item category) in backend

--- backend/category.py
+++ backend/category.py
@@ -3,3 +3,12 @@ class Category:
       self.__ID = ID
       self.__name = name
       self.__description = description
+   def __str__(self):
+      return f"ID: {self.__ID}\nName: {self.__name}\nDescription: {self.__description}"
+   
+   @property
+   def get_id(self):
+      return self.__ID
+   @property
+   def get_name(self):
+      return self.__name
--- backend/system.py
+++ backend/system.py
@@ -1,8 +1,7 @@
-# from .item import Item
-# from .user_class import User
-from .discount_code import Code
-from .category import Category
-from .item import *
+from discount_code import Code
+from category import Category
+from item import *
+import uuid
 
 class System:
    def __init__(self):
@@ -10,10 +9,75 @@ class System:
       self.__list_users:list[User] = []
       self.__list_codes:list[Code] = []
       self.__list_categories:list[Category] = []
+   
+   #function for validation duplicate name in list of instances
+   def __validate_name(self,name:str,instance:list[object]) -> bool:
+      for item in instance:
+         if item.get_name  == name:
+            return False
+      return True
       
-   def search_item(self,query:str = ''):
-      #return list of items
-      pass
+   #function to get list of instace
+   def get_categories(self,query:str = '') -> list[Category]:
+      if(query == ''): return self.__list_categories
+      else: return [category for category in self.__list_categories if query in category.get_name]
+   
+   def get_item(self,query:str = ''):
+      if(query == ''):
+         return self.__list_items
+      else:
+         return [item for item in self.__list_items if query in item.get_name]
+      
+   def get_users(self,query:str = ''):
+      if(query == ''):
+         return self.__list_users
+      else:
+         return [user for user in self.__list_users if query in user.get_name]
+   def get_user_by_id(self,id:str):
+      for user in self.__list_users:
+         if user.get_user_id == id:
+            return user
+      return None
+   
+   #function to create instace
+   def create_category(self, id:str,name:str, description:str):
+      if not self.__validate_name(name,self.__list_categories): raise Exception('Name already exist')
+      self.__list_categories.append(Category(id,name,description))
+      return 'Category created'
+   
+   def create_admin(self,name, user_id, email, phone_number, username, password, birth_date, gender):
+      if not self.__validate_name(username,self.__list_users): raise Exception('Name already exist') 
+      self.__list_users.append(Admin(name, user_id, email, phone_number, username, password, birth_date,gender))
+      return 'Admin created'
+   
+   def create_customer(self,name, user_id, email, phone_number, username, password, birth_date,gender,address:str,e_bux:float):
+      cart = Cart()
+      self.__list_users.append(Customer(name, user_id, email, phone_number, username, password, birth_date,gender,address,e_bux,cart))
+      return 'Customer created'
+   
+   def create_seller(self,customer:Customer,store_name:str,store_address:str):
+      for i in range(len(self.__list_users)):
+         if self.__list_users[i].get_user_id == customer.get_user_id and isinstance(self.__list_users[i],Customer):
+            self.__list_users[i] = Seller(customer,store_name,store_address)
+            return 'Seller created'  
+      
+   def create_discount_code(self, name:str, discount_percent:float):
+      self.__list_codes.append(Code(name,discount_percent))
+      return 'Code created'
+      
+   def create_item(self,current_user_id:str,name:str, price:float, amount:int,category_id:list[str],img=''):
+      if not self.__validate_name(name,self.__list_items): raise Exception('Item already exist')
+      current_user = self.get_user_by_id(current_user_id)
+      if current_user == None: raise Exception('User not found')
+      if not isinstance(current_user,Seller): raise Exception('User is not a seller')
+      category_list:list[Category] = []
+      for id in category_id:
+         for category in self.__list_categories:
+            if category.get_id == id:
+               category_list.append(category)
+      if len(category_list) == 0: raise Exception('Category not found')
+      self.__list_items.append(Item(uuid.uuid4(),name,price,amount,current_user,category_list,img))
+      return 'Item created'
    
    def view_item(self,itemId:str):
       #return item
@@ -32,9 +96,6 @@ class System:
    def save_discount_code(self,ID, discount_percent):
       pass
 
-   def create_category(self, name:str, description:str):
-      self.__list_categories.append(Category(name,description))
-      return 'Category created'
 
    def show_success_message():
       return "So good "
@@ -50,4 +111,31 @@ class System:
 
    def retrieve_order_data() :
       pass
-print('test gay')
+
+
+def createInstance():
+   from mock.items import items
+   from mock.category import categories
+   from mock.users import users
+   main_system = System()
+   #create category
+   [main_system.create_category(category['id'],category['name'],category['description']) for category in categories]
+   categories_instane = main_system.get_categories()
+   [print(category) for category in categories_instane]
+   #create user
+   for user in users:
+      if user['role'] == 'admin':
+         main_system.create_admin(user['name'],user['user_id'],user['email'],user['phone_number'],user['username'],user['password'],user['birth_date'],user['gender'])
+      elif user['role'] == 'customer' or user['role'] == 'seller':
+         main_system.create_customer(user['name'],user['user_id'],user['email'],user['phone_number'],user['username'],user['password'],user['birth_date'],user['gender'],user['address'],user['e_bux'])
+         if user['role'] == 'seller':
+            main_system.create_seller(main_system.get_user_by_id(user['user_id']),user['store_name'],user['store_address'])
+   users_instance = main_system.get_users()
+   [print(user) for user in users_instance]
+   #create item
+   for item in items:
+      main_system.create_item('sell001',item['name'],item['price'],item['amount'],['1','2'],item['image'])
+   items_instance = main_system.get_item()
+   [print(item) for item in items_instance]
+
+main_system = createInstance()
