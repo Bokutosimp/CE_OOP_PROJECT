8ff987d96683e4a8d649ed325622a2afa07df175
B0KUT0 <dead2548@gmail.com>
2025-03-07 13:36:10 +0700

    Link BidItem to bidpage butstill a bug

--- frontend/bid_page.py
+++ frontend/bid_page.py
@@ -4,13 +4,13 @@ from backend.system import main_system
 
 def bid_page(id):
    try:
-      item = main_system.get_bid_item_by_id(id)
+      bid_item = main_system.get_bid_item_by_id(id)
       return Div(
-            Div(Img(src=item.get_image), style="width:50%; height:auto; border-radius:15px; overflow:hidden;"),
+            Div(Img(src=bid_item.get_image), style="width:50%; height:auto; border-radius:15px; overflow:hidden;"),
             Div(
-               Div(H2(item.get_name, style="color:black; font-weight:600;"), style="width:100%; border-bottom:1px solid gray; margin-bottom:20px;"),
+               Div(H2(bid_item.get_name, style="color:black; font-weight:600;"), style="width:100%; border-bottom:1px solid gray; margin-bottom:20px;"),
                Div(style="height:40px; width:100%; border-bottom:1px solid gray;"),
-               Div(P(f"Current bids price ${item.get_price}", style="color:black; font-size:30px; font-weight:bold;"), style="width:100%;"),
+               Div(P(f"Current bids price ${bid_item.get_price}", style="color:black; font-size:30px; font-weight:bold;"), style="width:100%;"),
                Div(
                   Input(id="bid_input", style="width:flex; margin:0; text-align:center; border-radius:50px;", placeholder='Enter your bid'),
                    style="display:flex; flex-direction:row; gap:10px; width:100%; align-items:center;"),
