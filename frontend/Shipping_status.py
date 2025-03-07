51ddfaa5fce33d2c5abd3e8f46e4603146417493
Nanachi <arnonjantavara@gmail.com>
2025-03-07 23:29:14 +0700

    try change shipping status

--- frontend/Shipping_status.py
+++ frontend/Shipping_status.py
@@ -1,22 +1,26 @@
 from fasthtml.common import *
 from mock.items import items
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
+from backend.system import main_system
+
 
 def check_status(id):
     try:
         id = int(id)
-        for item in items:
-            if item["id"] == id:
-                return Div(
-                    H1("Shipping Status"),
-                    Div(
-                        Img(src=item["image"], style="width: 100px; height: 100px; border-radius: 50%;"),
-                        H2("Order ID: " + str(id), style="color : #000000;"),
-                        H3("Item: " + item["name"], style="color : #000000;"),
-                        H3("Status: Shipped", style="color : #000000;"),
-                        style="background-color: #EEEEEE; padding: 10px; border-radius: 5px; margin: 10px;"
-                    ),
-                    style="text-align: center; margin: 0 auto; padding: 10px;"
-                )
+        item = main_system.get_item_by_id(id)
+        return Div(
+            H1("Shipping Status"),
+            Div(
+                Img(src=item["image"], style="width: 100px; height: 100px; border-radius: 50%;"),
+                H2("Order ID: " + str(id), style="color : #000000;"),
+                H3("Item: " + item["name"], style="color : #000000;"),
+                H3("Status: Shipped", style="color : #000000;"),
+                style="background-color: #EEEEEE; padding: 10px; border-radius: 5px; margin: 10px;"
+            ),
+            style="text-align: center; margin: 0 auto; padding: 10px;"
+        )
         return Div("Item not found", style="text-align: center; margin: 0 auto; padding: 10px;")
     except Exception as e:
         return Div(f"Error: {str(e)}", style="text-align: center; margin: 0 auto; padding: 10px;")
