cbdf323cef032fc6aeae0bdd532a1b55f502edd3
B0KUT0 <dead2548@gmail.com>
2025-03-07 22:34:37 +0700

    link bid item to bid page

--- frontend/bid_page.py
+++ frontend/bid_page.py
@@ -1,22 +1,26 @@
 from fasthtml.common import *
-sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from backend.system import main_system
 
 def bid_page(id):
    try:
-      bid_item = main_system.get_bid_item_by_id(id)
+      bid_item = main_system.get_bid_item_by_id(int(id))
       return Div(
-            Div(Img(src=bid_item.get_image), style="width:50%; height:auto; border-radius:15px; overflow:hidden;"),
+         Div(Img(src=bid_item.get_image), style="width:50%; height:auto; border-radius:15px; overflow:hidden;"),
+         Form(
+            Div(H2(bid_item.get_name, style="color:black; font-weight:600;"), style="width:100%; border-bottom:1px solid gray; margin-bottom:20px;"),
+            Div(style="height:40px; width:100%; border-bottom:1px solid gray;"),
+            Div(P(f"Current bid price: ${bid_item.get_price}", style="color:black; font-size:30px; font-weight:bold;"), style="width:100%;"),
             Div(
-               Div(H2(bid_item.get_name, style="color:black; font-weight:600;"), style="width:100%; border-bottom:1px solid gray; margin-bottom:20px;"),
-               Div(style="height:40px; width:100%; border-bottom:1px solid gray;"),
-               Div(P(f"Current bids price ${bid_item.get_price}", style="color:black; font-size:30px; font-weight:bold;"), style="width:100%;"),
-               Div(
-                  Input(id="bid_input", style="width:flex; margin:0; text-align:center; border-radius:50px;", placeholder='Enter your bid'),
-                   style="display:flex; flex-direction:row; gap:10px; width:100%; align-items:center;"),
-               Button('Bid', style="width:100%; border-radius:50px;"),
-               style="display:flex; flex-direction:column; gap:10px; width:50%; align-items:center;"
-               ),
-            style="display:flex; flex-direction:row; width:100%; padding:30px; gap:20px;")
+               Span('Your Bid:', style="color:black; font-size:15px;"),
+               Input(id="bid_input",type='number',
+                     style="width:100px; margin:0; text-align:center; border-radius:50px;", placeholder='Enter your bid'),
+               style="display:flex; flex-direction:row; gap:10px; width:100%; align-items:center;"
+            ),
+            Button('Place Bid', style="width:100%; border-radius:50px;", type='submit'),
+            style="display:flex; flex-direction:column; gap:10px; width:50%; align-items:center;",
+         ),
+         style="display:flex; flex-direction:row; width:100%; padding:30px; gap:20px;"
+      )
    except:
       return Div("Item not found")
